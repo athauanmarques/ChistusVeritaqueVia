a667d68e3800fa09d1d95c76c7228d718be29ffd
AthauanPc <estovirksk@gmail.com>
2020-09-21 20:10:44 -0300

    Definição estrutural

---
+++ app/src/main/java/com/example/tercochistusveritaquevia/Activity/AgradecimentoActivity.java
@@ -0,0 +1,16 @@
+package com.example.tercochistusveritaquevia.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tercochistusveritaquevia.R;
+
+public class AgradecimentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agradecimento);
+    }
+}
---
+++ app/src/main/java/com/example/tercochistusveritaquevia/Activity/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.tercochistusveritaquevia.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tercochistusveritaquevia.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+/* TODO Implementação do SplashScreen
+* */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
