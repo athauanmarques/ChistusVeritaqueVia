217118f01cbafb89c965512b598f2c4fdc8b28c7
AthauanPc <estovirksk@gmail.com>
2020-09-26 21:19:42 -0300

    Anotações da lógica semanal para exibir
    Signed-off-by: AthauanPc <estovirksk@gmail.com>

--- app/src/main/java/com/example/tercochistusveritaquevia/controle/MisteriosSemanal.java
+++ app/src/main/java/com/example/tercochistusveritaquevia/controle/MisteriosSemanal.java
@@ -70,7 +70,15 @@ public class MisteriosSemanal extends ProgressoTerco {
     }
 }
 
+ /*Dias da Semana representado pelo sistema
+        1=Domingo, 2=Segunda, 3=Terça, 4=Quarta, 5=Quinta, 6=sexta, 7=Sábado
 
+        Mistérios
+            Segunda/Sábados = Mistérios da Alegria (2-7)
+            Terça/Sextas = Mistérios da Dor (3-6)
+            Quarta/Domingos = Mistérios da Glória (4-1)
+            Quintas-feiras = Mistérios da Luz (5)
+        * */
 
 
 
