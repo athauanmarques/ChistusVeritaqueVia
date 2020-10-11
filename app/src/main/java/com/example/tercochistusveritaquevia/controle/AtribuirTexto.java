5a60b38828f7af907b6d95f937b21a952614debb
AthauanPc <estovirksk@gmail.com>
2020-10-10 21:07:09 -0300

    Continuar terceira parte do terço

--- app/src/main/java/com/example/tercochistusveritaquevia/controle/AtribuirTexto.java
+++ app/src/main/java/com/example/tercochistusveritaquevia/controle/AtribuirTexto.java
@@ -1,5 +1,7 @@
 package com.example.tercochistusveritaquevia.controle;
 
+import android.content.Intent;
+
 import com.example.tercochistusveritaquevia.R;
 
 import static com.example.tercochistusveritaquevia.R.string.descricaoMt1Seg;
@@ -19,66 +21,152 @@ import static com.example.tercochistusveritaquevia.R.string.titulo5Mt;
 
 public class AtribuirTexto extends ProgressoTerco {
     private int titulo, img, subTitulo, oracao, exibirComponente = 2;
+    private String enderecoFoto;
 
     public void executarTerco(){
+
         contarTerco();
         if(getPosicaoTercoAtual() == 1) {
-            setTitulo(R.string.lblPaiNossoTitulo);
-            setSubTitulo(R.string.lblIncioSubTitulo);
-            setImg(R.drawable.p1);
-            setOracao(R.string.lblPaiNosso);
+            fixarTexto(5);// Pai Nosso
         } else if(getPosicaoTercoAtual() == 2) {
-            setTitulo(R.string.lblAveMariaTitulo);
-            setImg(R.drawable.p2);
-            setOracao(R.string.lblAveMaria);
+            fixarTexto(6);// Ave Maria
         } else if(getPosicaoTercoAtual() == 3){
             setImg(R.drawable.p3);
         } else if(getPosicaoTercoAtual() == 4){
             setImg(R.drawable.p4);
         } else if(getPosicaoTercoAtual() == 5){
+            fixarTexto(7);// Glória
+        } else if(getPosicaoTercoAtual() == 6){ //Primeiro Mistério
+            setExibirComponente(0); //exibir
+            atribuirMisterios(1);
+        } else if(getPosicaoTercoAtual() == 7) {
+            fixarTexto(1);// Pai Nosso
+        } else if(getPosicaoTercoAtual() >= 8 && getPosicaoTercoAtual() <= 17) {
+            fixarTexto(2);// Ave Maria
+        }else if(getPosicaoTercoAtual() == 18) {
+            fixarTexto(3);// Glória
+        } else if(getPosicaoTercoAtual() == 19) {
+            fixarTexto(4);// Jaculatoria
+        }else if(getPosicaoTercoAtual() == 20) { //Segundo Mistério
+            setExibirComponente(0); //exibir
+            atribuirMisterios(2);
+        }else if(getPosicaoTercoAtual() == 21) {
+            fixarTexto(1);
+        } else if(getPosicaoTercoAtual() >= 22 && getPosicaoTercoAtual() <= 31) {
+            fixarTexto(2);
+        }else if(getPosicaoTercoAtual() == 32) {
+            fixarTexto(3);
+        } else if(getPosicaoTercoAtual() == 33) {
+            fixarTexto(4);
+        }/*
+        else if(getPosicaoTercoAtual() == 34) {
+            //Terceiro Mistério
+            setExibirComponente(0); //exibir
+            atribuirMisterios(3);
+        }else if(getPosicaoTercoAtual() == 35) {
+            fixarTexto(1);
+        } else if(getPosicaoTercoAtual() >= 36 && getPosicaoTercoAtual() <= 45) {
+            fixarTexto(2);
+        }else if(getPosicaoTercoAtual() == 46) {
+            fixarTexto(3);
+        } else if(getPosicaoTercoAtual() == 47) {
+            fixarTexto(4);
+        }
+*/
+    }
+
+    public void fixarTexto(int op){
+        if(op == 1) {
+            setExibirComponente(2);
+            setTitulo(R.string.lblPaiNossoTitulo);
+            setImg(R.drawable.pai);
+            setOracao(R.string.lblPaiNosso);
+        } else if(op == 2) {
+            setTitulo(R.string.lblAveMariaTitulo);
+            mudarFoto();
+            setOracao(R.string.lblAveMaria);
+        } else if(op == 3){
+            setTitulo(R.string.lblGloriaTitulo);
+            mudarFoto();
+            setOracao(R.string.lblGloria);
+        } else if (op == 4){
+            setTitulo(R.string.lblJaculatoriaTitulo);
+            setImg(R.drawable.jaculatoria);
+            setOracao(R.string.lblJaculatoria);
+        } else if (op == 5){
+            setTitulo(R.string.lblPaiNossoTitulo);
+            setSubTitulo(R.string.lblIncioSubTitulo);
+            setImg(R.drawable.p1);
+            setOracao(R.string.lblPaiNosso);
+        } else if (op == 6){
+            setTitulo(R.string.lblAveMariaTitulo);
+            setImg(R.drawable.p2);
+            setOracao(R.string.lblAveMaria);
+        } else if (op == 7){
             setTitulo(R.string.lblGloriaTitulo);
             setImg(R.drawable.p5);
             setOracao(R.string.lblGloria);
         }
-        else if(getPosicaoTercoAtual() == 6){
-            setExibirComponente(0); //exibir
-            atribuirTextos();
-        } if(getPosicaoTercoAtual() == 7) {
-            setExibirComponente(2);
-            setTitulo(R.string.lblPaiNossoTitulo);
-            setImg(R.drawable.p1);
-            setOracao(R.string.lblPaiNosso);
+
+    }
+
+    public void mudarFoto(){
+        switch (getPosicaoTercoAtual()) {
+            //Primeira Parte Terço
+            case 8:  setImg(R.drawable.p6); break;
+            case 9:  setImg(R.drawable.p7); break;
+            case 10: setImg(R.drawable.p8); break;
+            case 11: setImg(R.drawable.p9); break;
+            case 12: setImg(R.drawable.p10); break;
+            case 13: setImg(R.drawable.p11); break;
+            case 14: setImg(R.drawable.p12); break;
+            case 15: setImg(R.drawable.p13); break;
+            case 16: setImg(R.drawable.p14); break;
+            case 17: setImg(R.drawable.p15); break;
+            case 18: setImg(R.drawable.p16); break; //Gloria
+            //Segunda Parte Terço
+            case 22: setImg(R.drawable.p17); break;
+            case 23: setImg(R.drawable.p18); break;
+            case 24: setImg(R.drawable.p19); break;
+            case 25: setImg(R.drawable.p20); break;
+            case 26: setImg(R.drawable.p21); break;
+            case 27: setImg(R.drawable.p22); break;
+            case 28: setImg(R.drawable.p23); break;
+            case 29: setImg(R.drawable.p24); break;
+            case 30: setImg(R.drawable.p25); break;
+            case 31: setImg(R.drawable.p26); break; //Gloria
+            case 32:  setImg(R.drawable.p27);break;
         }
     }
 
-    public void atribuirTextos(){
+    public void atribuirMisterios(int posicao){
         switch (diaHoje()) {
             case 2:
             case 7:
-                if(getPosicaoTercoAtual() == 6){
+                if(posicao == 1){
                     setTitulo(R.string.titulo1Mt);
                     setSubTitulo(subTituloMt1Seg);
                     setImg(R.drawable.segmt1);
                     setOracao(descricaoMt1Seg);
-                } else if(getPosicaoTercoAtual() ==18){
+                } else if(posicao == 2){
                     setTitulo(titulo2Mt);
                     setSubTitulo(subTituloMt2Seg);
-                    // setImg(R.drawable.segmt2);
+                    setImg(R.drawable.segmt2);
                     setOracao(descricaoMt2Seg);
-                } else if(getPosicaoTercoAtual() == 31){
+                } else if(posicao == 3){
                     setTitulo(titulo3Mt);
                     setSubTitulo(subTituloMt3Seg);
-                    // setImg(R.drawable.segmt3);
+                    setImg(R.drawable.segmt3);
                     setOracao(descricaoMt3Seg);
-                } else if(getPosicaoTercoAtual() == 44){
+                } else if(posicao == 4){
                     setTitulo(titulo4Mt);
                     setSubTitulo(subTituloMt4Seg);
-                    // setImg(R.drawable.segmt4);
+                    setImg(R.drawable.segmt4);
                     setOracao(descricaoMt4Seg);
-                }else if(getPosicaoTercoAtual() == 57){
+                }else if(posicao == 5){
                     setTitulo(titulo5Mt);
                     setSubTitulo(subTituloMt5Seg);
-                    // setImg(R.drawable.segmt5);
+                    setImg(R.drawable.segmt5);
                     setOracao(descricaoMt5Seg);
                 }
 
@@ -143,5 +231,13 @@ public class AtribuirTexto extends ProgressoTerco {
 
     public void setExibirComponente(int exibirComponente) {
         this.exibirComponente = exibirComponente;
+    }
+
+    public String getEnderecoFoto() {
+        return enderecoFoto;
+    }
+
+    public void setEnderecoFoto(String enderecoFoto) {
+        this.enderecoFoto = enderecoFoto;
     }
 }
