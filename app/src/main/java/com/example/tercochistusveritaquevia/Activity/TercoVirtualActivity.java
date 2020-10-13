70f7b36cb829f96b6c6619733231c8660c2d14c5
AthauanPc <estovirksk@gmail.com>
2020-10-13 16:25:10 -0300

    TercoVirtualActivity :  Atribuir os textos subTitulo,imagem e descrição, feito metodo da aplicação

--- app/src/main/java/com/example/tercochistusveritaquevia/Activity/TercoVirtualActivity.java
+++ app/src/main/java/com/example/tercochistusveritaquevia/Activity/TercoVirtualActivity.java
@@ -22,16 +22,31 @@ import static androidx.appcompat.app.AlertDialog.*;
 public class TercoVirtualActivity extends IntroActivity {
 
     AtribuirTexto atribuir = new AtribuirTexto();
-    private ImageView imgMt1;
-    private TextView Mt1SubTituloTV, Mt1DescricaoTV ;
-    Fragment frMt1 = new Fragment();
+    private ImageView imgMt1, imgMt2, imgMt3, imgMt4, imgMt5;
+    private TextView Mt1SubTituloTV, Mt2SubTituloTV, Mt3SubTituloTV, Mt4SubTituloTV, Mt5SubTituloTV,
+                     Mt1DescricaoTV, Mt2DescricaoTV, Mt3DescricaoTV, Mt4DescricaoTV, Mt5DescricaoTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mt1DescricaoTV = findViewById(R.id.txtMt1SubTitulo);
-        Mt1DescricaoTV = findViewById(R.id.txtMt1DescricaoTV);
+
         imgMt1 = findViewById(R.id.imgMt1TV);
+        imgMt2 = findViewById(R.id.imgMt2TV);
+        imgMt3 = findViewById(R.id.imgMt3TV);
+        imgMt4 = findViewById(R.id.imgMt4TV);
+        imgMt5 = findViewById(R.id.imgMt5TV);
+        Mt1SubTituloTV = findViewById(R.id.txtMt1SubTitulo);
+        Mt2SubTituloTV = findViewById(R.id.txtMt2SubTitulo);
+        Mt3SubTituloTV = findViewById(R.id.txtMt3SubTitulo);
+        Mt4SubTituloTV = findViewById(R.id.txtMt4SubTitulo);
+        Mt5SubTituloTV = findViewById(R.id.txtMt5SubTitulo);
+        Mt1DescricaoTV = findViewById(R.id.txtMt1DescricaoTV);
+        Mt2DescricaoTV = findViewById(R.id.txtMt2DescricaoTV);
+        Mt3DescricaoTV = findViewById(R.id.txtMt3DescricaoTV);
+        Mt4DescricaoTV = findViewById(R.id.txtMt4DescricaoTV);
+        Mt5DescricaoTV = findViewById(R.id.txtMt5DescricaoTV);
+
+       // misterioFixar();
 
         //Ocultar botões padrões do slide
         setButtonBackVisible(false);
@@ -85,11 +100,26 @@ public class TercoVirtualActivity extends IntroActivity {
     }
 
     //TODO Atribuir os textos subTitulo,imagem e descrição
-    public void misterioFixar(int ordemMisterio){
-        atribuir.atribuirMisterios(ordemMisterio);
-        imgMt1.setImageResource(atribuir.getImg());
-        Mt1SubTituloTV.setText(atribuir.getSubTitulo());
-        Mt1DescricaoTV.setText(atribuir.getOracao());
+    public void misterioFixar(){
+
+        atribuir.atribuirMisterios();
+        imgMt1.setImageResource(atribuir.getImgMt1Tv());
+        imgMt2.setImageResource(atribuir.getImgMt2Tv());
+        imgMt3.setImageResource(atribuir.getImgMt3Tv());
+        imgMt4.setImageResource(atribuir.getImgMt4Tv());
+        imgMt5.setImageResource(atribuir.getImgMt5Tv());
+
+        Mt1SubTituloTV.setText(atribuir.getSubTituloMt1Tv());
+        Mt2SubTituloTV.setText(atribuir.getSubTituloMt2Tv());
+        Mt3SubTituloTV.setText(atribuir.getSubTituloMt3Tv());
+        Mt4SubTituloTV.setText(atribuir.getSubTituloMt4Tv());
+        Mt5SubTituloTV.setText(atribuir.getSubTituloMt5Tv());
+
+        Mt1DescricaoTV.setText(atribuir.getDescricaoMt1Tv());
+        Mt2DescricaoTV.setText(atribuir.getDescricaoMt2Tv());
+        Mt3DescricaoTV.setText(atribuir.getDescricaoMt3Tv());
+        Mt4DescricaoTV.setText(atribuir.getDescricaoMt4Tv());
+        Mt5DescricaoTV.setText(atribuir.getDescricaoMt5Tv());
     }
 
     public void aplicarTexto(){
