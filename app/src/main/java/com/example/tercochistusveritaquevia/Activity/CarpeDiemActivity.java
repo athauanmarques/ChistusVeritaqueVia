d9204f3f76b162673e275533e44f26cdb8617257
athauan-dev <athauan.marques.dev@gmail.com>
2021-12-14 23:06:47 -0300

    implementação Carpe Diem e Sobre

---
+++ app/src/main/java/com/example/tercochistusveritaquevia/Activity/CarpeDiemActivity.java
@@ -0,0 +1,14 @@
+package com.example.tercochistusveritaquevia.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarpeDiemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carpe_diem);
+    }
+}
